556f9c24582e72462506427446e85465fa4b83d0
Hanif Hashim <hanifabdul123@hotmail.com>
2023-09-10 13:41:11 -0400

    Activity 02: Drew an amazing alien

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Draw an Alien
+ * Hanif Hashim
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Using p5’s drawing instructions to draw an alien
  */
 
 "use strict";
@@ -17,9 +16,40 @@ function preload() {
 
 
 /**
- * Description of setup
+ * Drawing different parts of the Alien
 */
 function setup() {
+
+    // Draw Canvas and setting up background color
+    createCanvas(640, 480);
+    background(255, 192, 203);
+
+    //Draw & fill head
+    noStroke();
+    fill(220, 220, 220);
+    ellipse(320,240,300, 450);
+
+    //Draw & fill eyes
+    fill(0,0,0)
+    ellipse(200,320,100)
+    ellipse(350, 320, 100)
+    
+    //Draw and fill nostrils
+    
+    fill('rgba(0,0,0, 0.7)');
+    ellipse(250,400,20);
+    ellipse(285, 400, 20);
+    
+    //Draw and fill mouth
+    fill(219,112,147)
+    stroke(255, 50, 122);
+    strokeWeight(5);
+    rectMode(CENTER);
+    rect(280,440,75,25);
+
+
+    
+
 
 }
 
