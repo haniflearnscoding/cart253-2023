9764eb6f8304ba7c6212c928022cbef1295ee6a4
Hanif Hashim <hanifabdul123@hotmail.com>
2023-09-18 19:59:01 -0400

    A3: Completed the activity!

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -22,16 +22,20 @@ let circle1 = {
     x: 0,
     y: 250,
     size: 100,
+    growthRate: 1,
     fill: 255,
-    alpha: 200
+    alpha: 200,
+    speed:1
 
 }
 let circle2 = {
     x: 500,
     y: 250,
     size: 50,
+    sizeRatio: 0.75,
     fill: 255,
-    alpha: 200
+    alpha: 200,
+    speed: -1
 
 }
 
@@ -59,8 +63,29 @@ function setup() {
 
 
 /**
- * Description of draw()
+ * Description of draw
 */
 function draw() {
+    // background
+    background(bg.r, bg.g, bg.b);
+    bg.r = map(circle1.size,100,width,0,255);
+
+    //circle 1
+    circle1.x = circle1.x + circle1.speed;
+    circle1.x = constrain(circle1.x, 0, width / 2);
+    circle1.size = circle1.size + circle1.growthRate; 
+    circle1.size = constrain(circle1.size,0,width);
+    fill(circle1.fill, circle1.alpha);
+    ellipse(circle1.x, circle1.y, circle1.size);
+
+    //circle 2
+    circle2.x = circle2.x + circle2.speed
+    circle2.x = constrain(circle2.x, width / 2, width);
+    circle2.size = circle1.size * circle2.sizeRatio;
+    fill(circle2.fill, circle2.alpha);
+    ellipse(circle2.x, circle2.y, circle2.size);
+
+    
+
 
 }
