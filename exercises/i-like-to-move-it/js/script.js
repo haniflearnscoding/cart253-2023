fe772476e3a8d2ef9716c63c1f6765b1fa915ecd
Hanif Hashim <hanifabdul123@hotmail.com>
2023-09-18 23:15:00 -0400

    Declare all my variables as js objects

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -1,12 +1,67 @@
 /**
- * Title of Project
- * Author Name
+ * Exercise: I like to move it!
+ * Hanif Hashim
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * 
+ * 
  */
 
 "use strict";
+
+let bg = {
+    r: 0,
+    g: 0,
+    b: 0
+        
+}
+    
+let circle1 = {
+    x: 0,
+    y: 250,
+    size: 100,
+    growthRate: 1,
+    fill: 255,
+    alpha: 200,
+    speed:1
+
+}
+let circle2 = {
+    x: 500,
+    y: 250,
+    size: 50,
+    sizeRatio: 0.75,
+    fill: 255,
+    alpha: 200,
+    speed: -1
+
+}
+
+let circle3 = {
+    x: 250,
+    y: 250,
+    size: 50,
+    fill: 0
+    
+
+}
+
+let rect1 = {
+    x: 250,
+    y: 500,
+    size: 150,
+    speed: -1,
+    growthRate: 1
+}
+
+let tri1 = {
+    x1: 250,
+    y1: 0,
+    x2: 0,
+    y2: 500,
+    x3: 500,
+    y3: 500
+
+}
 
 /**
  * Description of preload
