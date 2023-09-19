a229b34966ecb404c81558cbdbe1c976612e9a3f
Hanif Hashim <hanifabdul123@hotmail.com>
2023-09-18 23:16:45 -0400

    Draw my circles

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -95,4 +95,27 @@ function draw() {
     bg.g += 10;
     bg.b += 10;
 
+    //circle 1
+   
+    circle1.x = circle1.x + circle1.speed;
+    circle1.x = constrain(circle1.x, 0, width / 2);
+    circle1.size = circle1.size + circle1.growthRate; 
+    circle1.size = constrain(circle1.size,0,width);
+    fill(circle1.fill, circle1.alpha);
+    ellipse(circle1.x, circle1.y, circle1.size);
+
+    //circle 2
+    circle2.x = circle2.x + circle2.speed
+    circle2.x = constrain(circle2.x, width / 2, width);
+    circle2.size = circle1.size * circle2.sizeRatio;
+    fill(circle2.fill, circle2.alpha);
+    ellipse(circle2.x, circle2.y, circle2.size);
+
+    //circle 3
+    ellipse(circle3.x, circle3.y, circle3.size);
+    circle3.x = mouseX;
+    circle3.y = mouseY;
+    circle3.fill = map(mouseX, 0, width, 0, 255);
+    fill(circle3.fill);
+
 }
