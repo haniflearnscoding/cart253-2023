010f651266c71bd3c8750957f338d2fefea9c2f3
Hanif Hashim <hanifabdul123@hotmail.com>
2023-09-22 21:30:14 -0400

    Learned about Booleans

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,13 +1,8 @@
 
 "use strict";
 
-let backgroundShade = 0;
-let circle = {
-    x: 0,
-    y: 250,
-    size: 100,
-    speed: 1
-}
+let displayCircle = false;
+
 
 /**
  * Description of preload
@@ -30,19 +25,15 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(backgroundShade);
-    
-    circle.x = circle.x + circle.speed;
 
-    fill(255,255,255);
+    background(0);
 
-    if (!(circle.x < width / 3) ) { 
-        
-            fill(255,0,0);
-        
+    if (mouseIsPressed) { 
+        displayCircle = true;
+    }
+
+    if (displayCircle) { 
+        ellipse(250,250,100,100);
     }
     
-    
-    ellipse(circle.x,circle.y,circle.size);
-
 }
