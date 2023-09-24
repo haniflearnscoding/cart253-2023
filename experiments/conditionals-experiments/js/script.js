5fc434e51c684d0500ea42643c0e1264b7e10fcc
Hanif Hashim <hanifabdul123@hotmail.com>
2023-09-23 23:43:53 -0400

    Learned about movement through velocity and acceleration

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,13 +1,13 @@
-let bg = {
-    r: 0,
-    g: 0,
-    b: 0
-}
-
 let circle = {
     x: 250,
     y: 250,
-    size: 100
+    size: 100,
+    vx: 1,
+    vy: 0,
+    ax: 0,
+    ay: 0,
+    acceleration: 0.25,
+    maxSpeed: 5
 }
 
 
@@ -31,16 +31,29 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    background(bg.r,bg.g,bg.b);
+    background(0);
 
-    // circle.x = mouseX;
-    // circle.y = mouseY;
+    if (mouseX < circle.x) {
+        circle.ax = -circle.acceleration;
+    }
+    else { 
+        circle.ax = circle.acceleration;
+    }
 
-    ellipse(circle.x, circle.y, circle.size);
-}
+    if (mouseY < circle.y) {
+        circle.ay = -circle.acceleration;
+    
+    }
+    else { 
+        circle.ay = circle.acceleration;
+    }
 
-function mouseDragged() { 
-    bg.r = random(0,255);
-    bg.g = random(0,255);
-    bg.b = random(0,255);
+    circle.vx = circle.vx + circle.ax;
+    circle.vx = constrain(circle.vx, -circle.maxSpeed,circle.maxSpeed);
+    circle.vy = circle.vy + circle.ay;
+
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+
+    ellipse(circle.x,circle.y,circle.size);
 }
