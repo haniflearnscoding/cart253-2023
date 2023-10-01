9b3a4295a5ae58dcc727a16aa915aa4cfda234cf
Hanif Hashim <hanifabdul123@hotmail.com>
2023-10-01 12:05:56 -0400

    Learned about setting up different functions to reduce repeating code

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -28,6 +28,42 @@ function setup() {
 function draw() {
     background(0);
 
-    ellipse(width/2,height/2,100,100);
+    move();
+    wrap();
+    display();
+}
+/**
+ * Defining Move function
+*/
+function move() { 
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+}
+/**
+ * Defining Wrap function
+*/
+function wrap() { 
+    if (circle.x > width) { 
+        reset();
+    }
+}
+/**
+ * Defining Reset Function
+*/
+function reset() { 
+    circle.x = 0;
+    circle.vx = circle.vx + 1;
+    circle.size = circle.size + 5;
+}
+/**
+ * Defining Display Function
+*/
 
+function display() { 
+    fill(255, 0, 0);
+    ellipse(circle.x,circle.y,circle.size);
+}
+
+function mousePressed() { 
+    reset();
 }
