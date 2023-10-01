639fcc2a7bd26b535158a5fb89d5ea619a078aa1
Hanif Hashim <hanifabdul123@hotmail.com>
2023-10-01 12:19:53 -0400

    Learned about parameters

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -2,68 +2,53 @@
  * Description of preload
 */
 
-let circle = {
-    x: 0,
-    y: 250,
-    size: 100,
-    vx: 1,
-    vy: 0
-}
+// let circle = {
+//     x: 0,
+//     y: 250,
+//     size: 100,
+//     vx: 1,
+//     vy: 0
+// }
 function preload() {
 
 }
-
-
 /**
  * Description of setup
 */
+
 function setup() {
     createCanvas(500,500);
 }
-
-
 /**
  * Description of draw()
 */
+
 function draw() {
     background(0);
+    parallels(0, 200, 100, 1, 100, 4);
+    parallels(0, 300, 20, 10, 50, 12);
+    parallels(0, 350, 80, 5, 5, 6);
 
-    move();
-    wrap();
-    display();
-}
-/**
- * Defining Move function
-*/
-function move() { 
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy;
-}
-/**
- * Defining Wrap function
-*/
-function wrap() { 
-    if (circle.x > width) { 
-        reset();
-    }
-}
-/**
- * Defining Reset Function
-*/
-function reset() { 
-    circle.x = 0;
-    circle.vx = circle.vx + 1;
-    circle.size = circle.size + 5;
-}
-/**
- * Defining Display Function
-*/
-
-function display() { 
-    fill(255, 0, 0);
-    ellipse(circle.x,circle.y,circle.size);
 }
 
-function mousePressed() { 
-    reset();
+// function parallels(x,y) { 
+//     for (let i = 0; i < 20; i++) { 
+//         noStroke();
+//         fill(255);
+//         rectMode(CENTER);
+//         rect(x, y, 2, 50);
+//         x += 5;
+//     }
+// }
+
+
+function parallels(x, y, numLines, lineThickness, lineHeight, lineSpacing) {
+  for (let i = 0; i < numLines; i++) {
+    noStroke();
+    let lineFill = map(i, 0, numLines, 0, 255);
+    fill(lineFill);
+    rectMode(CENTER);
+    rect(x, y, lineThickness, lineHeight);
+    x = x + lineSpacing;
+  }
 }
