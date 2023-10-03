b8a7342e2d4e6cd3cd87332bee8891839fcc5fe9
Hanif Hashim <hanifabdul123@hotmail.com>
2023-10-02 20:15:04 -0400

    Learned about states

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,18 +1,64 @@
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2,
+}
+
+let state = `title`; // title, animation, ending
+
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
+  circle.vx = circle.speed;
+
+  textSize(32);
+  textAlign(CENTER, CENTER);
 }
 
 function draw() {
   background(0);
 
-  textAlign(CENTER, CENTER);
-  textSize(64);
-  textStyle(BOLD);
-
-  fill(200, 100, 200);
-  stroke(0);
-  strokeWeight(2);
-  
-  text(`Hello, World!`,250,250);
+  if (state === `title`){
+    title();
+  }else if 
+  (state === `animation`){
+    animation();    
+  }else if (state === `ending`){
+    ending(); 
+  }
 }
 
+function title() { 
+  // Title
+    fill(255);
+    text(`Life`, width / 2, height / 2);
+}
+
+function animation() { 
+  // Animation
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+
+    if (circle.x > width) {
+      state = `ending`;
+  } 
+  ellipse(circle.x, circle.y, circle.size);
+}
+
+function ending() { 
+  // Ending
+    fill(127);
+    text(`It's all over.`, width / 2, height / 2)
+}
+
+
+
+function keyPressed() {
+  // If any key is pressed, we check if the current state is the title state
+  if (state === `title`) {
+    // If it is, we switch to the animation state
+    state = `animation`;
+  }
+}
