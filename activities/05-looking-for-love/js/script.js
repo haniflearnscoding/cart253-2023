550ff902add07dffc5921a78f1a26536a70a25d8
Hanif Hashim <hanifabdul123@hotmail.com>
2023-10-02 22:54:05 -0400

    Finished refactoring all my functions

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -55,4 +55,42 @@ function setup() {
 function draw() {
     background(0);
 
+    
+}
+
+function simulation() { 
+    move();
+    checkOffScreen();
+    checkOverlap();
+    display();
+}
+
+function move() { 
+    //move and draw
+    circle1.x = circle1.x + circle1.vx;
+    circle1.y = circle1.y + circle1.vy;
+    
+    circle2.x = circle2.x + circle2.vx;
+    circle2.y = circle2.y + circle2.vy;
+}
+
+function checkOffScreen() { 
+    //check if circles off canvas
+    if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height) { 
+        //SAD ENDING
+    }
+}
+
+function checkOverlap() { 
+    //check if circles overlap
+    let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+    if (d < circle1.size / 2 + circle2.size / 2) { 
+        //LOVE ENDING
+    }
+}
+
+function display() { 
+    //display
+    ellipse(circle1.x, circle1.y, circle1.size);
+    ellipse(circle2.x, circle2.y, circle2.size);
 }
