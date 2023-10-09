d68603380b02515fdc041fb83c6604fe3cd04b37
Hanif Hashim <hanifabdul123@hotmail.com>
2023-10-09 19:56:37 -0400

    Found assets to use and declared variables

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,18 +1,69 @@
 /**
- * Title of Project
- * Author Name
+ * Gardening Simulator
+ * Hanif Hashim
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ *Gardening simulator using js p5 library
+
  */
 
 "use strict";
 
+// objects for all plants in garden
+let eggplant = {
+    x: 100,
+    y: 100,
+    size: 100,
+    image: undefined
+};
+
+let lemon = {
+    x: 100,
+    y: 100,
+    size: 100,
+    image: undefined
+};
+let mushroom = {
+    x: 100,
+    y: 100,
+    size: 100,
+    image: undefined
+};
+let potato = {
+    x: 100,
+    y: 100,
+    size: 100,
+    image: undefined
+};
+let tangerine = {
+    x: 100,
+    y: 100,
+    size: 100,
+    image: undefined
+};
+let tomato = {
+    x: 100,
+    y: 100,
+    size: 100,
+    image: undefined
+};
+let watermelon = {
+    x: 100,
+    y: 100,
+    size: 100,
+    image: undefined
+};
+
 /**
- * Description of preload
+ * loading all garden plants
 */
 function preload() {
-
+    eggplant.image = loadImage('assets/images/');
+    lemon.image = loadImage('assets/images/');
+    mushroom.image = loadImage('assets/images/');
+    potato.image = loadImage('assets/images/');
+    tangerine.image = loadImage('assets/images/');
+    tomato.image = loadImage('assets/images/');
+    watermelon.image = loadImage('assets/images/');
 }
 
 
