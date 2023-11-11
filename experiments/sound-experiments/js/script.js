388f5124a4f0febeffeb8c755473d5f324165d4a
Hanif Hashim <hanifabdul123@hotmail.com>
2023-11-10 22:05:54 -0500

    Played with getPeaks() and addCue()

--- experiments/sound-experiments/js/script.js
+++ experiments/sound-experiments/js/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 let barkSFX;
+// Whether to display "BARK!"
+let showBarkText = false;
 
 function preload() {
     barkSFX = loadSound(`assets/sounds/bark.wav`);
@@ -9,22 +11,40 @@ function preload() {
 function setup() {
     createCanvas(600, 600);
     userStartAudio();
+
+    // Add cues to our sound at specific times (in seconds)
+    // which will call either showBark() or hideBark() in order
+    // to only show the text during barking sounds...
+    barkSFX.addCue(0.1, showBark);
+    barkSFX.addCue(0.3, hideBark);
+    barkSFX.addCue(0.4, showBark);
+    barkSFX.addCue(0.7, hideBark);
 }
 
 function draw() {
     background(0);
 
-    // We can calculate the rate we should set the sound to by mapping
-    // the mouse's x position to our desired range..
-    let newRate = map(mouseX, 0, width, -3, 3);
-    // And then set the rate of our sound file to the new rate
-    // Note how we can do this while the sound is still playing!
-    barkSFX.rate(newRate);
+    // If showBarkText is true, we should display BARK! on the canvas
+    if (showBarkText) {
+        push();
+        fill(255);
+        textSize(64);
+        textAlign(CENTER, CENTER);
+        text(`BARK!`, width / 2, height / 2);
+        pop();
+    }
+}
+
+// Called when the appropriate cue is triggered!
+function showBark() {
+    showBarkText = true;
+}
+
+// Called when the appropriate cue is triggered!
+function hideBark() {
+    showBarkText = false;
 }
 
 function mousePressed() {
-    // Since we'll be playing around with the sound, it makes sense to loop it
-    // so it doesn't stop.
-    barkSFX.loop();
-
+    barkSFX.play();
 }
