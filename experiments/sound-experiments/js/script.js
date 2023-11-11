74f490c97ac2935031d400091f47486bf722d104
Hanif Hashim <hanifabdul123@hotmail.com>
2023-11-11 13:36:00 -0500

    Learned about PolySynth

--- experiments/sound-experiments/js/script.js
+++ experiments/sound-experiments/js/script.js
@@ -1,33 +1,52 @@
 "use strict";
 
-let oscillator; // To store our oscillator
-let t = 0; // The t (time) value to use with noise()
-let tIncrease = 0.075; // How much to increase t each frame
+// Our synthesizer
+let synth;
+// The scale for F minor ("b" means "flat" if you haven't seen it before)
+let notes = [`F4`, `G4`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F5`];
+// The current note to play, start at the beginning
+let currentNote = 0;
+// To track the interval that plays note
+let interval;
 
 function setup() {
     createCanvas(600, 600);
     userStartAudio();
 
-    // Create a new oscillator
-    oscillator = new p5.Oscillator(0, `sine`);
-    // Set its amplitude down a bit or this could hurt
-    oscillator.amp(0.25);
+    // Create the synthesizer
+    synth = new p5.PolySynth();
 }
 
 function draw() {
     background(0);
-
-    // Generate a Perlin noise value based on our t value
-    let perlinValue = noise(t);
-    // Map the result (between 0 and 1) to a frequency range
-    let newFreq = map(perlinValue, 0, 1, 110, 880);
-    // Set the frequency of the oscillator based on the Perlin value
-    oscillator.freq(newFreq);
-    // Increase t
-    t += tIncrease;
 }
 
-// mousePressed() starts our siren
+// mousePressed() starts and stops our piano playing
 function mousePressed() {
-    oscillator.start();
+    // First check that the piano isn't already playing
+    // The interval will be undefined if it hasn't started
+    if (interval === undefined) {
+        // Start our interval, calling playNextNote every 500 milliseconds
+        // Assign the result to interval to remember the interval
+        interval = setInterval(playNextNote, 500);
+    }
+    else {
+        // If they click when it's playing, clear the interval and set interval
+        // back to undefined to stop play
+        clearInterval(interval);
+        interval = undefined;
+    }
+}
+
+// playNextNote() plays the next note in our array
+function playNextNote() {
+    // Chose the note at the current position
+    let note = notes[currentNote];
+    // Play it
+    synth.play(note, 0.2, 0, 0.4);
+    // Increase the current position and go back to 0 when we reach the end
+    currentNote = currentNote + 1;
+    if (currentNote === notes.length) {
+        currentNote = 0;
+    }
 }
