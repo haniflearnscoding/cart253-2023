da10731cab9b241d3cbf80f04036fa21a80a99c8
Hanif Hashim <hanifabdul123@hotmail.com>
2023-11-12 20:22:32 -0500

    Activity: FInished the sound activity

--- activities/sound-activity/js/script.js
+++ activities/sound-activity/js/script.js
@@ -1,12 +1,16 @@
 /**
- * Title of Project
- * Author Name
+ * Musical Toy
+ * Hanif Hashim
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * A program that plays music based on primitive physics.
  */
 
 "use strict";
+
+let balls = [];
+
+// F-minor
+let notes = [`F3`, `G3`, `Ab4`, `Bf4`, `C4`, `Db4`, `Eb4`, `F4`];
 
 /**
  * Description of preload
@@ -20,6 +24,9 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(600, 600);
+
+    userStartAudio();
 
 }
 
@@ -28,5 +35,22 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    for (let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+        ball.move();
+        ball.bounce();
+        ball.display();
+    }
+}
+
+function mousePressed() {
+    createBall(mouseX, mouseY);
+}
+
+function createBall(x, y) {
+    let note = random(notes);
+    let ball = new Ball(x, y, note);
+    balls.push(ball);
 }
