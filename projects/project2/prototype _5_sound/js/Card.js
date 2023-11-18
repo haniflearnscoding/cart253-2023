557688cd31b257ac1a9d03c7d203a85181ccd0f3
Hanif Hashim <hanifabdul123@hotmail.com>
2023-11-17 22:50:03 -0500

    Created a sound prototype

---
+++ projects/project2/prototype _5_sound/js/Card.js
@@ -0,0 +1,61 @@
+class Card {
+    constructor(x, y, suite) {
+        this.x = x;
+        this.y = y;
+        this.w = 50;
+        this.h = 70;
+        this.rows = 4;
+        this.cols = 3;
+        this.suite = suite; // Store the suite of the card
+        // this.char = char; // Store the character associated with the suite
+        this.flipped = false;
+        this.checked = false;
+    }
+    // display() draws our card onto the canvas
+    display() {
+        push();
+        if (this.flipped) {
+            fill(255); // Black if flipped
+        } else {
+            fill(0); // White if not flipped
+        } noStroke();
+        // rectMode(CENTER);
+        rect(this.x, this.y, this.w, this.h);
+
+        // Display the suite text in the center of the card
+        textFont(cardFont);
+        textSize(20);
+        textStyle(NORMAL);
+        fill(0);
+        textAlign(CENTER, CENTER);
+        text(this.suite, this.x + this.w / 2, this.y + this.h / 2);
+        pop();
+
+    }
+
+    cardFlip() {
+        this.flipped = !this.flipped;
+        // console.log("Card flipped:", this.flipped);
+    }
+
+    mousePressed() {
+
+        // Loop through all the cards and check if the mouse is over each card
+        for (let i = 0; i < table.cards.length; i++) {
+            let card = table.cards[i];
+            if (
+                mouseX > card.x &&
+                mouseX < card.x + card.w &&
+                mouseY > card.y &&
+                mouseY < card.y + card.h
+            ) {
+                // console.log("Mouse pressed on card:", card);
+
+                // Toggle the pressed state of the card
+                if (!card.flipped) {
+                    card.cardFlip();
+                }
+            }
+        }
+    }
+}
---
+++ projects/project2/prototype _5_sound/js/script.js
@@ -0,0 +1,244 @@
+/**
+ * Project 2: Table Minigame!
+ * Hanif Hashim
+ * 
+ *Table minigame inspired by Super Mario Bros. DS, using js p5 library
+ */
+
+"use strict";
+
+// Our table
+
+let table = {
+    // An array to store the individual flowers
+    cards: [],
+    // How many cards are on the table
+    numCards: 2,
+    // The color of the table (background)
+    tableColor: {
+        r: 53,
+        g: 101,
+        b: 77
+    },
+    suites: [`Hearts`, `Clubs`, `Spades`, `Diamonds`]
+};
+
+let flippedCards = [];
+
+let state = `title`; // title, simulation, end
+
+
+// Font used for characters on card
+let cardFont;
+
+let rows = 3;
+let cols = 4
+
+let w = 50;
+let h = 70;
+
+
+
+// Preload function
+function preload() {
+    cardFont = loadFont(`assets/fonts/C64_Pro_Mono-STYLE.otf`);
+
+}
+
+// Setup function
+function setup() {
+    createCanvas(700, 400);
+    // rect(c * this.w, r * this.h, this.w, this.h);
+
+    // Create Deck
+    for (let i = 0; i < rows * cols; i += 2) {
+        let suite = random(table.suites);
+        // Assign a character based on your custom font mapping
+        let char;
+        switch (suite) {
+            case 'Hearts':
+                char = '\u2665'; // Replace with the actual character for hearts
+                break;
+            case 'Clubs':
+                char = '\u2663'; // Replace with the actual character for clubs
+                break;
+            case 'Spades':
+                char = '\u2660'; // Replace with the actual character for clubs
+                break;
+            case 'Diamonds':
+                char = '\u2666'; // Replace with the actual character for clubs
+                break;
+            default:
+                char = ''; // Default character if not specified
+        }
+
+        // Create a new card using the arguments
+        let card = new Card(undefined, undefined, char)
+        // let card2 = new Card(x2, y2, char)
+        // Add the card to the array of cards
+        table.cards.push(card);
+        card = new Card(undefined, undefined, char)
+        table.cards.push(card);
+    }
+
+    table.cards.sort(function (a, b) {
+        return 0.5 - Math.random();
+    });
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            // Calculate the center of the canvas
+            let centerX = width / 2;
+            let centerY = height / 2;
+
+            // Calculate the starting position for the cards
+            let startX = centerX - (cols / 2) * w * 1.25;
+            let startY = centerY - (rows / 2) * h * 1.25;
+
+            // Create our cards by counting up to the number of the cards
+            let x = startX + (c * w) * 1.25;
+            let y = startY + (r * h) * 1.25;
+
+            let cardIndex = c + r * cols;
+            table.cards[cardIndex].x = x;
+            table.cards[cardIndex].y = y;
+        }
+    }
+}
+
+
+// Draw function, different states of the game
+function draw() {
+    if (state === `title`) {
+        title();
+    }
+    else if (state === `simulation`) {
+        simulation();
+    }
+    else if (state === `end`) {
+        end();
+    }
+}
+
+function title() {
+    push();
+    //set the bg img white
+    background(255);
+
+    //text settings 
+    textSize(22);
+    fill(0);
+    textAlign(CENTER);
+
+    //text content & placement
+    text(`Placeholder`, width / 2, height / 2);
+    pop();
+}
+
+function end() {
+    push();
+    //set the bg img white
+    background(255);
+
+    //text settings 
+    textSize(22);
+    fill(0);
+    textAlign(CENTER);
+
+    //text content & placement
+    text(`Placeholder`, width / 2, height / 2);
+    pop();
+}
+
+function simulation() {
+    // Display the table
+    background(table.tableColor.r, table.tableColor.g, table.tableColor.b);
+    displayCards();
+}
+
+// Flower function
+function displayCards() {
+    // Loop through all the cards in the array and display them
+    for (let i = 0; i < table.cards.length; i++) {
+        let card = table.cards[i];
+
+        card.display();
+    }
+}
+
+// Mouse Click function, move from title screen to simulation
+function mouseClicked() {
+    if (state === `title`) {
+        state = `simulation`;
+    }
+}
+
+function mousePressed() {
+    // Loop through all the cards and check if the mouse is over each card
+    for (let i = 0; i < table.cards.length; i++) {
+        let card = table.cards[i];
+        card.mousePressed();
+    }
+
+    matchCard();
+
+    resetFlip();
+}
+
+
+function matchCard() {
+    // Array to store flipped cards
+
+    // Loop through all the cards to find flipped ones
+    for (let i = 0; i < table.cards.length; i++) {
+        let card = table.cards[i];
+
+        // Check if the card is flipped
+        if (card.flipped && flippedCards.length < 2 && !flippedCards.includes(card) && card.checked === false) {
+            // Add the flipped card to the array
+            flippedCards.push(card);
+
+            // console.log("Flipped card:", card);
+        }
+    }
+
+}
+
+function resetFlip() {
+    console.log("resetFlip function called"); // Add this line
+
+    // Check if there are exactly two flipped cards
+    if (flippedCards.length === 2) {
+        // console.log(flippedCards.length);
+
+        // let shouldFlipBack = true; // Flag to determine whether any card should be flipped back
+
+        // Check if the suites of the two flipped cards are the same
+        if (flippedCards[0].suite === flippedCards[1].suite) {
+            flippedCards[0].checked = true;
+            flippedCards[1].checked = true;
+            // Cards have the same suite, do something (e.g., remove the matched cards)
+            console.log("Matched!");
+        } else {
+            // Cards have different suites, flip them back or take other actions
+            console.log("Not matched!");
+
+            // if (shouldFlipBack) {
+
+            for (let i = 0; i < table.cards.length; i++) {
+                let card = table.cards[i];
+                console.log(`test`);
+                if (flippedCards.includes(card)) {
+                    setTimeout(() => {
+                        card.cardFlip(); // Flip back only the cards in flippedCards array
+                    }, 500);
+                }
+            }
+            // }
+        }
+
+        // Reset the array of flipped cards for the next turn
+        flippedCards = [];
+    }
+
+}
